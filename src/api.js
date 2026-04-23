596ca326bce524a2772d2bf394de82daa225920c
fuzelusmani-glitch <fuzelusmai@gmail.com>
2026-04-24 00:28:05 +0530

    h

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mindcare-backend-71f2.onrender.com/api",
+  baseURL: "https://mindcare-backend-s8pc.onrender.com/api",
   headers:{
     "Content-type":"application/json",
   },
